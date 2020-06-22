1c4e2f09c8042462ca2ae791bf82ea037e085093
Luka Doric <luka.doric@uns.ac.rs>
2020-06-22 19:06:22 +0200

    dodati su repozitorijumi koji su falili

---
+++ project-generated-code-backend/Backend/Repository/AddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Repository;
+using Model.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI_SIMS_PROJEKAT.Backend.Repository
+{
+    public interface AddressRepository : GenericRepository<Address>
+    {
+    }
+}
